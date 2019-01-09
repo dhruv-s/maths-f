1e423c1baa2d0f9a16e1f4f3ee4bfcd316a2deac
Dhruv Suthar <intelligentjimmy@gmail.com>
2019-01-09 16:22:25 +0530

    Update falseposition.py

--- algebra/falseposition.py
+++ algebra/falseposition.py
@@ -3,30 +3,10 @@
 # Website: dhrvsthr.ml
 # Python 3.7.2
 
-import math
-def f(x):
-    return x**3 - (5*x) -6
+# Function 'f(x)' returns the functional value at 'x'
+# Function 'opp(a,b)' returns Boolean value based on sign of 'a' and 'b' checking if opposite or not
+# Function 'flps(fn,lf,rg)' returns the midpoint after assert areopp() and further computing midpoint
 
-def opp(a,b):
-    return a*b<0
-
-def falsepos(fn,lf,rg):
-    assert opp(fn(lf),fn(rg))
-    for i in range(7):
-$ head falseposition.py 12
-==> falseposition.py <==
-import math
-def f(x):
-    return x**3 - (5*x) -6
-
-def opp(a,b):
-    return a*b<0
-
-def falsepos(fn,lf,rg):
-    assert opp(fn(lf),fn(rg))
-    for i in range(7):
-head: cannot open '12' for reading: No such file or directory
-$ head -n 30 falseposition.py
 import math
 def f(x):
     return x**3 - (5*x) -6
@@ -45,5 +25,5 @@ def falsepos(fn,lf,rg):
         print("x",i+1," f(x) = ",f(md),"[",lf,rg,"]","[",f(lf),f(rg),"]")
     return md
 
-x = falsepos(f,2,3)
+x = flps(f,2,3)
 print(x)
