e9056096eace61870029a56319b29498e9b5338e
Dhruv Suthar <intelligentjimmy@gmail.com>
2019-01-09 22:24:03 +0530

    Final

--- algebra/bisectionmethod.py
+++ algebra/bisectionmethod.py
@@ -10,27 +10,33 @@ import math
 # Function 'f(x)' returns the functional value at 'x'
 # Function 'areopp(a,b)' returns Boolean value based on sign of 'a' and 'b'
 # Function 'bisect(fn,lf,rg)' returns the midpoint after assert areopp() and further computing midpoint
+# 'bsct()' function uses Bisection Method for Root Finding
 
-## Function Definitions
+## Function Definitions and IMmportant Variables
 
-def f(x):   
-    return (x**3)-(3*x)-5
-    # [any equation in terms of 'x'] 
+# [any valid integer for iterations]
+nit = 25
+
+def f(x):
+    return x - math.cos(x)
+    # [any equation in terms of 'x']
 
 def areopp(a,b):
     return a*b<0
 
-def bisect(fn,lf,rg):
-    assert areopp(fn(lf),fn(rg))
-    for i in range(256):
-        md = (lf+rg)/2.000
-        if(areopp(fn(lf),fn(rg))):
+def bsct(lf,rg):
+    assert areopp(f(lf),f(rg))
+    for i in range(nit):
+        md = (lf+rg)/2.0
+        print(md)
+        if(areopp(f(lf),f(md))):
             rg=md
         else:
             lf=md
+        # add print statement for traversal of values
     return md
 
 ## Main Program Run
 
-x = bisect(f,2,3)
+x = bsct(0.5,1)
 print(x)
