5d71123885249c0afe7b400e964b852908e24d65
Dhruv Suthar <intelligentjimmy@gmail.com>
2019-01-09 22:24:39 +0530

    Final

--- algebra/falseposition.py
+++ algebra/falseposition.py
@@ -1,29 +1,40 @@
 """ a program to implement false position method for finding root """
-# Dhruv Suthar [ github/dhrvsthr ]
+# Dhruv Suthar [ Github/dhrvsthr ]
 # Website: dhrvsthr.ml
 # Python 3.7.2
 
-# Function 'f(x)' returns the functional value at 'x'
-# Function 'opp(a,b)' returns Boolean value based on sign of 'a' and 'b' checking if opposite or not
-# Function 'flps(fn,lf,rg)' returns the midpoint after assert areopp() and further computing midpoint
+# importing 'math' module
 
 import math
-def f(x):
-    return x**3 - (5*x) -6
 
-def opp(a,b):
+# Function 'f(x)' returns the functional value at 'x'
+# Function 'areopp(a,b)' returns Boolean value based on sign of 'a' and 'b' checking if opposite or not
+# Function 'flps(fn,lf,rg)' returns the midpoint after assert areopp() and further computing midpoint
+# 'flps()' function uses False Position Method for Root Finding
+
+## Function Definitions and IMmportant Variables
+
+# [any valid integer for iterations]
+nit = 10
+
+def f(x):
+    return x - math.cos(x)
+    # [any equation in terms of 'x']
+
+def areopp(a,b):
     return a*b<0
 
-def falsepos(fn,lf,rg):
-    assert opp(fn(lf),fn(rg))
-    for i in range(7):
-        md = ((lf*fn(rg))-(rg*fn(lf)))/(fn(rg)-fn(lf))
-        if(opp(fn(lf),fn(md))):
+def flps(lf,rg):
+    assert areopp(f(lf),f(rg))
+    for i in range(nit):
+        md = ((lf*f(rg))-(rg*f(lf)))/(f(rg)-f(lf))
+        print(md)
+        if(areopp(f(lf),f(md))):
             rg=md
         else:
             lf=md
-        print("x",i+1," f(x) = ",f(md),"[",lf,rg,"]","[",f(lf),f(rg),"]")
+        # add print statement for traversal of values
     return md
 
-x = flps(f,2,3)
+x = flps(0.5,1)
 print(x)
